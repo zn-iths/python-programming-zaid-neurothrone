89c014fd47b954c848e9d0c042ac1cb6711195a1
Zaid Neurothrone <zaid.neurothrone@protonmail.com>
2021-09-25 03:21:03 +0200

    replace validator functions with decorator class

--- labs/lab_03/validation.py
+++ labs/lab_03/validation.py
@@ -1,11 +1,57 @@
-def validate_input(obj_name: str, values: list[float]) -> None:
-    for value in values:
-        if not isinstance(value, (float, int)):
-            raise TypeError(f"{obj_name} object was provided invalid input."
-                            f" Only float and int is supported.")
+from functools import wraps
+from typing import Callable
 
 
-def validate_scalars(obj_name: str, scalars: list[float]) -> None:
-    for scalar in scalars:
-        if scalar <= 0:
-            raise ValueError(f"{obj_name} object was provided a negative or zero value.")
+# TODO: attempt to refactor code (repeated three times)
+
+class Validator:
+    def __init__(self, func: Callable) -> None:
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        return self.func(*args, **kwargs)
+
+    @staticmethod
+    def init_args_type(exclude: bool = False, indices: list[int] = None):
+        def decorator(func: Callable):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                args_ = kwargs.values()
+                if exclude:
+                    args_ = [arg for index, arg in enumerate(args_) if index not in indices]
+                for value in args_:
+                    if not isinstance(value, (float, int)):
+                        raise TypeError(f"Invalid input. Only float and int is supported.")
+                return func(*args, **kwargs)
+            return wrapper
+        return decorator
+
+    @staticmethod
+    def init_args_negative(exclude: bool = False, indices: list[int] = None):
+        def decorator(func: Callable):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                args_ = kwargs.values()
+                if exclude:
+                    args_ = [arg for index, arg in enumerate(args_) if index not in indices]
+                for value in args_:
+                    if value < 0:
+                        raise ValueError(f"Value is negative.")
+                return func(*args, **kwargs)
+            return wrapper
+        return decorator
+
+    @staticmethod
+    def args_type(exclude: bool = False, indices: list[int] = None):
+        def decorator(func: Callable):
+            @wraps(func)
+            def wrapper(*args, **kwargs):
+                args_ = args
+                if exclude:
+                    args_ = [arg for index, arg in enumerate(args) if index not in indices]
+                for value in args_:
+                    if not isinstance(value, (float, int)):
+                        raise TypeError(f"Invalid input. Only float and int is supported.")
+                return func(*args, **kwargs)
+            return wrapper
+        return decorator
