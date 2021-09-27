695fb1d097b88c626d26055f9662887409250a0d
Zaid Neurothrone <zaid.neurothrone@protonmail.com>
2021-09-27 11:31:45 +0200

    add test for polymorphism

--- labs/lab_03/tests.py
+++ labs/lab_03/tests.py
@@ -1,4 +1,5 @@
 from math import pi
+from typing import Union
 import unittest
 
 from circle import Circle
@@ -10,6 +11,7 @@ from shape import Shape
 class TestShapeClasses(unittest.TestCase):
     def test_base_cases(self):
         """Test base cases of requirement specification."""
+
         c1 = Circle(x=0, y=0, radius=1)
         c2 = Circle(x=1, y=1, radius=1)
         r1 = Rectangle(x=0, y=0, width=1, length=1)
@@ -21,6 +23,7 @@ class TestShapeClasses(unittest.TestCase):
 
     def test_validation(self):
         """Validation tests when passing negative or zero values and strings."""
+
         with self.assertRaises(ValueError):
             c1 = Circle(x=0, y=0, radius=0)
             c2 = Circle(x=0, y=0, radius=-5)
@@ -34,6 +37,7 @@ class TestShapeClasses(unittest.TestCase):
 
     def test_translation(self):
         """Test subclasses translation method."""
+
         c1 = Circle(x=0, y=0, radius=2)
         start = c1.position.values
         c1.translate(2, 2)
@@ -49,6 +53,7 @@ class TestShapeClasses(unittest.TestCase):
 
     def test_math_computations(self):
         """Test computations of subclasses circumference and area methods."""
+
         r1 = Rectangle(x=0, y=0, width=2, length=2)
         c1 = Circle(x=0, y=0, radius=2)
         self.assertEqual(2 * 2, r1.area())
@@ -56,10 +61,37 @@ class TestShapeClasses(unittest.TestCase):
         self.assertEqual(pi * c1.radius ** 2, c1.area())
         self.assertEqual(pi * 2 * c1.radius, c1.circumference())
 
+    def test_polymorphism(self):
+        """Test polymorphism of Shape subclasses without raising an exception."""
+
+        # tests that should fail
+        for value in ("woop", -5):
+            with self.assertRaises((TypeError, ValueError)):
+                self.polymorphism_test_variance(test_variable=value)
+
+        # test that should succeed
+        try:
+            self.polymorphism_test_variance(5)
+        except (TypeError, ValueError):
+            self.fail(f"{self.test_polymorphism().__name__} test failed.")
+
+    @staticmethod
+    def polymorphism_test_variance(test_variable: Union[float, int, str]) -> None:
+        shapes = [
+            Circle(x=0, y=0, radius=test_variable),
+            Circle(x=1, y=1, radius=1),
+            Rectangle(x=0, y=0, width=1, length=1),
+            Rectangle(x=0, y=0, width=2, length=2)
+        ]
+
+        for shape in shapes:
+            print(shape.area())
+            print(shape.circumference())
+
 
 def main():
     unittest.main()
 
+
 if __name__ == "__main__":
     main()
-
